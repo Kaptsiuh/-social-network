f8c1845223eb8ff412b6e4869ffb9d88f48a7d06
Dima Berry <dimaberry1@gmail.com>
2024-02-28 12:01:36 +0100

    complete components 07

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <Header />
+      <Technologies />
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <div>
+      <a href="#1">Home</a>
+      <a href="#2">News Feed</a>
+      <a href="#3">Messages</a>
+    </div>
+  );
+};
+
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>js</li>
+        <li>html</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
