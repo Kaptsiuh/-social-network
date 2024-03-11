40760facc752070604d7578a8e8ba6f791adbfd9
Dima Berry <dimaberry1@gmail.com>
2024-03-11 20:14:04 +0100

    add content in dialog components 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,5 +1,20 @@
 import s from "./Dialogs.module.css";
 
 export const Dialogs = () => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItem}>
+        <div className={s.dialog + " " + s.active}>Dmitry</div>
+        <div className={s.dialog}>Valera</div>
+        <div className={s.dialog}>Kris</div>
+        <div className={s.dialog}>Ola</div>
+        <div className={s.dialog}>Semen</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>Hi Hi</div>
+        <div className={s.message}>How are u?</div>
+      </div>
+    </div>
+  );
 };
