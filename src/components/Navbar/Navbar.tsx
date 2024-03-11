869608b3d30011027b981c547f425771448dfa9f
Dima Berry <dimaberry1@gmail.com>
2024-03-11 16:00:08 +0100

    add router 19

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,27 +5,27 @@ export const Navbar = () => {
     <nav className={s.nav}>
       <ul>
         <li>
-          <a className={`${s.item} ${s.active}`} href="#1">
+          <a className={`${s.item} ${s.active}`} href="/profile">
             Profile
           </a>
         </li>
         <li>
-          <a className={s.item} href="#1">
+          <a className={s.item} href="/dialogs">
             Messages
           </a>
         </li>
         <li>
-          <a className={s.item} href="#1">
+          <a className={s.item} href="/news">
             News
           </a>
         </li>
         <li>
-          <a className={s.item} href="#1">
+          <a className={s.item} href="/music">
             Music
           </a>
         </li>
         <li>
-          <a className={s.item} href="#1">
+          <a className={s.item} href="/settings">
             Settings
           </a>
         </li>
